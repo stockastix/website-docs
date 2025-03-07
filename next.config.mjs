c0511ecd8aae14de939f1cf96a027116b60fbb23
swiing <persovince@yahoo.fr>
2025-03-07 20:44:16 +0100

    fix: next configuration to load external images
    I had forgotten to commit this change together with 58af668

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dsotf1kzl/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default withMDX(config);
