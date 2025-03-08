8aabebef60d124b649defeedcd15406a76c0c953
swiing <persovince@yahoo.fr>
2025-03-08 09:55:37 +0100

    fix: correct rm command in postinstall script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,13 +8,13 @@
 cd ./content/docs
 # https://unix.stackexchange.com/a/207296
 ln -sfn ../../node_modules/@stockastix/technical-indicators/src/ technical-indicators
+# move back to root
+cd ../..
 # delete json files, otherwise it confuses fumadocs
 # https://stackoverflow.com/a/42655267/18612308
 rm -f ./content/docs/technical-indicators/**/*.test.json
 # otherwise npm run build complains because it tries to process the typescript files
-rm -f ./content/docs/technical-indicators/__test__
-# move back to root
-cd ../..
+rm -r -f ./content/docs/technical-indicators/__test__
 
 # run original postinstall from fumadocs
 fumadocs-mdx
