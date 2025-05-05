e719e170ff00c94902398c32be576a5a84f395ed
swiing <persovince@yahoo.fr>
2025-05-05 20:33:21 +0200

    build: copy @technical-indicators files instead of symlink
    It provides more flexibility in the sense that I can add additional
    documentation files in the tree structure if need be.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-# create symlink to @technical-indicators so that its content is taken into account
+# copy from @technical-indicators so that its content is taken into account
 # when creating documentation
-
-# I need to cd to the target directory where I want to create the symlink
-# otherwise the link points to the wrong place
-cd ./content/docs
-# https://unix.stackexchange.com/a/207296
-ln -sfn ../../node_modules/@stockastix/technical-indicators/src/ technical-indicators
-# move back to root
-cd ../..
+cp -a ./node_modules/@stockastix/technical-indicators/src/ technical-indicators
 # delete json files, otherwise it confuses fumadocs
 # https://stackoverflow.com/a/42655267/18612308
 rm -f ./content/docs/technical-indicators/**/*.test.json
