fd98338f1f8b15c61d2af9e410f0a814aee70e6d
swiing <persovince@yahoo.fr>
2025-06-08 22:05:37 +0200

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
+const GoogleAnalyticsMeasurementId = "G-BG7R43KT6T";
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html
@@ -16,6 +18,24 @@ export default function Layout({ children }: { children: ReactNode }) {
       data-color-mode="auto"
       data-light-theme="dark_high_contrast"
     >
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src={`"https://www.googletagmanager.com/gtag/js?id=${GoogleAnalyticsMeasurementId}"`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GoogleAnalyticsMeasurementId}');
+          `,
+          }}
+        />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
