295e64ee560c7b7bbf0e394cc0e6cf53e35bee11
swiing <persovince@yahoo.fr>
2025-06-15 14:52:26 +0200

    docs: rename 'technical indicators' in 'indicators' in the menu and add content

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,12 +2,12 @@
 
 # copy from @technical-indicators so that its content is taken into account
 # when creating documentation
-cp -a ./node_modules/@stockastix/technical-indicators/src/. ./content/docs/technical-indicators
+cp -a ./node_modules/@stockastix/technical-indicators/src/. ./content/docs/indicators
 # delete json files, otherwise it confuses fumadocs
 # https://stackoverflow.com/a/42655267/18612308
-rm -f ./content/docs/technical-indicators/**/*.test.json
+rm -f ./content/docs/indicators/**/*.test.json
 # otherwise npm run build complains because it tries to process the typescript files
-rm -r -f ./content/docs/technical-indicators/__test__
+rm -r -f ./content/docs/indicators/__test__
 
 # run original postinstall from fumadocs
 fumadocs-mdx
